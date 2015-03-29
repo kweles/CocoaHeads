3b259dea00c707d1e12936760700d91bba159f17
Weles Saggiorato Siqueira <kweles@gmail.com>
2015-03-29 18:54:57 -0300

    Exemplo Custom TableView
    Exemplo Custom TableView

---
+++ CustomTableViewCell/Cardapios/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Cardapios
+//
+//  Created by Weles Siqueira on 23/11/14.
+//  Copyright (c) 2014 Weles Siqueira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
